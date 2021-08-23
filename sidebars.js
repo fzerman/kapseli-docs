d8fce885e5ea6df9e587c2b7d56cd8c07742c471
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2021-08-24 00:26:14 +0300

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -4,17 +4,8 @@ module.exports = {
   ],
   api: {
      "Managers": [
-        'storage-manager',
-        'command-manager',
-        'data-manager',
-        'event-manager',
-        'form-manager',
-        'i18n',
-        'modal-manager',
-        'panel-manager',
-        'plugin-manager',
-        'route-manager',
-        'view-manager'
+        'managers/storage-manager',
+        'managers/command-manager'
      ]
   },
 };
