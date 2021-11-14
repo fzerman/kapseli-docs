94fc6a2636ffb67572cb6010249414fb1fc7a136
Furkan Zerman <59289764+Swindler36@users.noreply.github.com>
2021-11-15 02:03:22 +0300

    Update support.js

--- src/pages/support.js
+++ src/pages/support.js
@@ -55,19 +55,7 @@ function Support() {
           </div>
           <div className="col col--12">
             <h2>Buy Me a Coffee</h2>
-            <script
-              type="text/javascript"
-              src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-              data-name="bmc-button"
-              data-slug="stingydev"
-              data-color="#FFDD00"
-              data-emoji=""
-              data-font="Cookie"
-              data-text="Buy me a coffee"
-              data-outline-color="#000000"
-              data-font-color="#000000"
-              data-coffee-color="#ffffff"
-            ></script>
+            <a href="https://www.buymeacoffee.com/stingydev" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
           </div>
           <div className="col col--12">
             <h2>Kreosus</h2>
